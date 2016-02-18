1ed2dce9f2b14229060235ec778b1a29a6241c9a
Kevin Haggerty <kevin.haggerty@havasww.com>
2016-02-18 16:42:28 -0500

    frequency analysis solution with better support for multiple winners

--- index.js
+++ index.js
@@ -6,7 +6,42 @@ var solver = function() {
 	return () => "solved!";
 }
 
+var frequent = function(arr) {
+    var winners = [], freq = {}, highest = 0;
+    for (var i in arr) {
+        var val = arr[i];
+        // console.log('val', val);
+        // simple frequency counter
+        if (!freq[val]) freq[val] = 0;
+        freq[val]++;
+        // if a new high point
+        if (freq[val] >= highest) {
+            // we have a new potential winner
+            highest = freq[val];
+            winners.push({val: val, freq: freq[val]});
+            // if multiple winners, any previous winner that is less than highest should be eliminated
+            if (winners.length > 1) {
+                // reduce to a new array
+                winners = winners.reduce(function(previous, item, i, whole){
+                    // console.log('reducing:', item, item.freq, previous, highest);
+                    if (item.freq === highest) {
+                        previous.push(item);
+                        return previous;
+                    }
+                    return previous;
+                }, []);
+                // console.log('reduced:', winners);
+            }  
+        } 
+    }
+    
+    winners = winners.map(function(item){ return item.val });
+    console.log('done', freq, highest, winners);
+    return winners;
+
+}
 
 module.exports = {
-	solver: solver
+	solver: solver,
+    frequent: frequent
 }
--- test/tests.js
+++ test/tests.js
@@ -5,24 +5,42 @@ var chai = require('chai'),
 
 var lib = require('../index');
 
-describe('testing the test suite', function() {
-	it('should be true', function() {
-		assert.equal(true, !false, "not false should be true");
-	});
-	it('should not be false', function() {
-		assert.notEqual(true, false, "false should not be true");
-	});
-});
+describe('a mode function', function() {
 
-describe('test the solver', function() {
-	var solver = lib.solver();
-	it('should return a function', function(){
-		assert.isFunction(solver, "solver is a function");
+    it ('should find the single most frequent value', function(){
+        assert.deepEqual(lib.frequent([0,1,1,1,1,2,6,7,3,2,5,6,6,6,6,9]), [ 6 ]);
+    })
+    
+    it ('should find the multiple most frequent value', function(){
+        assert.deepEqual(lib.frequent([1,2,3]), [ 1, 2, 3 ]);
+    });
+    it ('should find another multiple most frequent value', function(){
+        assert.deepEqual(lib.frequent([1,2,3, 44, 44, 44, 55, 55, 55]), [ 44, 55 ]);
+    });
+	it ('should work with different types', function(){
+		assert.deepEqual(lib.frequent([1,2,3, "44", "44", "44", "55", "55", "55", 1.2345, 1.2345, 1.2345]), [ "44", "55", 1.2345 ]);
 	});
-	it('should return a string when called', function(){
-		assert.isString(solver(), "solver() is a string");
-	});
-	it('should return the correct string', function(){
-		assert.equal(solver(), "solved!", "solver() is a string");
-	});
-});
+
+})
+
+// describe('testing the test suite', function() {
+// 	it('should be true', function() {
+// 		assert.equal(true, !false, "not false should be true");
+// 	});
+// 	it('should not be false', function() {
+// 		assert.notEqual(true, false, "false should not be true");
+// 	});
+// });
+
+// describe('test the solver', function() {
+// 	var solver = lib.solver();
+// 	it('should return a function', function(){
+// 		assert.isFunction(solver, "solver is a function");
+// 	});
+// 	it('should return a string when called', function(){
+// 		assert.isString(solver(), "solver() is a string");
+// 	});
+// 	it('should return the correct string', function(){
+// 		assert.equal(solver(), "solved!", "solver() is a string");
+// 	});
+// });
